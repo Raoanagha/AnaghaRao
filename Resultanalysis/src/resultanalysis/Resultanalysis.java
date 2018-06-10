3d7d32b963e02df607708075bec8944fea1a8a1c
User <User@DESKTOP-V5OFSCA>
2018-06-10 11:14:03 +0530

    javadoc version 3

--- Resultanalysis/src/resultanalysis/Resultanalysis.java
+++ Resultanalysis/src/resultanalysis/Resultanalysis.java
@@ -1,3 +1,4 @@
+
 package resultanalysis;
 
 
@@ -44,12 +45,23 @@ public static void main(String args[])
                break;
         case 4: c.classtopper();
                break;
-        case 5:c.mathtopper();
+        case 5: c.mathtopper();
                 c.mmctopper();
                 c.dbmstopper();
-         default:System.out.println("INVALID OPTION");
+                c.ostopper();
+                c.daatopper(); 
+               break;
+        case 6:c.classpass();
+               break;
+        case 7:c.subjpass();
+                break;
+        default:System.out.println("INVALID OPTION");
                 break;
     }
     }        
 }    
 }
+
+    
+
+
