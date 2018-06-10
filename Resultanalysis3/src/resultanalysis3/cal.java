e06db1c7f9c0af37013c5d3a1c817a7808d7b79e
User <User@DESKTOP-V5OFSCA>
2018-06-10 12:07:48 +0530

    source code version 2

--- Resultanalysis3/src/resultanalysis3/cal.java
+++ Resultanalysis3/src/resultanalysis3/cal.java
@@ -3,11 +3,6 @@ package resultanalysis3;
 
 import java.util.Scanner;
 
-/**
- *This is a public class containing all the methods
- * 
- * 
- */
 public class cal 
 {
             int i=0,j=0,n=0;
@@ -40,19 +35,7 @@ public class cal
             private String daatopper;
             private int daatopperroll;
             
-            /**
-             * This method takes input from the user<br>
-             * It collects information of all the students<br>
-             * n-number of students<br>
-             * name-name of the student<br>
-             * roll-roll no<br>
-             * math-marks scored in math<br>
-             * dbms-marks scored in dbms<br>
-             * mmc-marks scored in mmc<br>
-             * os-marks scored in os<br>
-             * daa-marks scored in daa<br>
-             * 
-             */
+            
             public void input()
             {
                     System.out.println("Enter the number of students: ");
@@ -86,9 +69,7 @@ public class cal
                     daa[i] = s.nextInt();
             }
             }
-            /**
-            *This method displays all the information collected 
-            */
+           
             
             public void display()
             {
@@ -103,10 +84,6 @@ public class cal
               }
             }
             
-            /**
-             *  This method calculates total marks obtained by each student<br>
-             * sum[]- An array to store total marks of each student<br>
-             */
             
             public void total()
             {
@@ -124,12 +101,7 @@ public class cal
           
             }
             }
-            /**
-             * This method finds out the student securing highest marks in the class<br>
-             * max- marks scored by the topper<br>
-             * topper-name of the topper<br>
-             * topperroll-rollno of the topper
-             */
+            
             public void classtopper()
             {
                
@@ -155,12 +127,192 @@ public class cal
                System.out.println("MARKS SCORED: "+max);
               
             }
-            /**
-             *This method finds out the student securing highest marks in math<br>
-             * mathmax-marks of the student securing highest marks in math<br>
-             * mathtopper-name of the student securing highest marks in math<br>
-             * mathtopperroll-rollno of the student securing highest marks in math
-             */
+            
              
+            public void mathtopper()
+            {
+                
+              
+           System.out.println("HIGHEST SCORER OF EVERY SUBJECT");
+           System.out.println("SUBJECT   " + "NAME  " + "    ROLL NO  " + "MARKS SCORED ");
+                 mathmax=math[0];
+                 
+                for ( i = 1; i < n; i++)
+            {
+                
+     		if (math[i]>mathmax)
+		 {
+			mathmax= math[i];
+                        mathtopper = name[i];
+                        mathtopperroll=roll[i];
+                 }
+                
+             }
+               System.out.println("MATH   " + "   "+mathtopper + "         "+mathtopperroll + "     "+mathmax );
+             
+            }
+            
+              public void mmctopper()
+            {
+                
+              
+               
+                 mmcmax=mmc[0];
+                 
+                for ( i = 1; i < n; i++)
+            {
+                
+     		if (mmc[i]>mmcmax)
+		 {
+			mmcmax= mmc[i];
+                        mmctopper = name[i];
+                        mmctopperroll=roll[i];
+                 }
+                
+             }
+                System.out.println("MMC    " + "   "+mmctopper + "         "+mmctopperroll + "     "+mmcmax );
+            }
+              
+                public void dbmstopper()
+            {
+               
+              
+               
+                 dbmsmax=math[0];
+                 
+                for ( i = 1; i < n; i++)
+            {
+                
+     		if (dbms[i]>dbmsmax)
+		 {
+			dbmsmax= dbms[i];
+                        dbmstopper = name[i];
+                        dbmstopperroll=roll[i];
+                 }
+                
+             }
+                System.out.println("DBMS   " + "   "+dbmstopper + "         "+dbmstopperroll + "     "+dbmsmax );
+            }
+                
+                  public void ostopper()
+            {
+              
+              
+               
+                 osmax=math[0];
+                 
+                for ( i = 1; i < n; i++)
+            {
+                
+     		if (os[i]>osmax)
+		 {
+			osmax= os[i];
+                        ostopper = name[i];
+                        ostopperroll=roll[i];
+                 }
+                
+             }
+               System.out.println("OS     " + "   "+ostopper + "         "+ostopperroll + "     "+osmax );
+            }
+                  
+                    public void daatopper()
+            {
+                
+              
+               
+                 daamax=daa[0];
+                 
+                for ( i = 1; i < n; i++)
+            {
+                
+     		if (daa[i]>daamax)
+		 {
+			daamax= daa[i];
+                        daatopper = name[i];
+                        daatopperroll=roll[i];
+                 }
+                
+             }
+               System.out.println("DAA    " + "   "+daatopper + "         "+daatopperroll + "     "+daamax );
+            }
+           
+            
+            public void classpass()
+            {
+                
+                for(i=0;i<n;i++)
+              {
+
+                  if(sum[i]>175)
+                  p++;
+              }
+                
+                 System.out.println("The percentage of students who failed in the class is: "+((n-p)*100)/n+"%");
+                 System.out.println("The percentage of students who passed in the class is: "+(p*100)/n+"%");
+            }
+            
+            
+            
+            public void subjpass()
+            {
+                 
+                 System.out.println("SUBJECT      " + "  FAIL   " + "PASS"); 
+                for(i=0;i<n;i++)
+                {
+                     if(math[i]<35)
+                     f1++;
+
+                }
+
+                 System.out.println("MATHS    : " + "    "+(f1*100)/n+"%" + "   " +((n-f1)*100)/n+"%");
+                
+                 
+                 
+
+                for(i=0;i<n;i++)
+                {
+                     if(mmc[i]<35)
+                     f2++;
+
+                }
+
+                 System.out.println("MMC      : "+"    "+(f2*100)/n+"%" + "   " +((n-f2)*100)/n+"%");
+                
+
+                 
+               
+                for(i=0;i<n;i++)
+                {
+                     if(dbms[i]<35)
+                     f3++;
+
+                }
+
+                System.out.println("DBMS     : "+"    "+(f3*100)/n+"%" + "   " +((n-f3)*100)/n+"%");
+
+                 
+                  /*total number of students who passed in subject os*/
+
+                for(i=0;i<n;i++)
+                {
+                     if(os[i]<35)
+                     f4++;
+
+                }
+
+               System.out.println("OS       : "+"    "+(f4*100)/n+"%" + "   " +((n-f4)*100)/n+"%");
+
+                 
+                 
+
+                for(i=0;i<n;i++)
+                {
+                     if(daa[i]<35)
+                     f5++;
+
+                }
+
+                 System.out.println("DAA      : "+"    "+(f5*100)/n+"%" + "   " +((n-f5)*100)/n+"%");
+            }
            
 }
